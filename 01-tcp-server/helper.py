5755685b45e60adbd161762d4dfe4f4cd73dc893
Lukáš Litvan <lukas.litvan@gmail.com>
2019-04-11 09:03:39 +0200

    Homework 01 - Add robot functionality - navigation, recharging, picking up treasure

--- 01-tcp-server/helper.py
+++ 01-tcp-server/helper.py
@@ -44,3 +44,105 @@ def compute_hash(username):
     for c in username:
         char_sum += ord(c)
     return (char_sum * 1000) % 65536
+
+
+def get_direction(coords1, coords2):
+    x1, y1 = coords1
+    x2, y2 = coords2
+
+    if x1 == x2 and y1 != y2:
+        if y1 < y2:
+            return UP
+        else:
+            return DOWN
+
+    elif x1 != x2 and y1 == y2:
+        if x1 < x2:
+            return RIGHT
+        else:
+            return LEFT
+
+    else:
+        return 4
+
+
+def get_next_move(position, direction):
+    x, y = position
+
+    if x > -2:
+        if direction == UP:
+            return SERVER_TURN_LEFT, position, LEFT
+        elif direction == DOWN:
+            return SERVER_TURN_RIGHT, position, LEFT
+        elif direction == RIGHT:
+            return SERVER_TURN_LEFT, position, UP
+        return SERVER_MOVE, (x-1, y), LEFT
+
+    elif x < -2:
+        if direction == UP:
+            return SERVER_TURN_RIGHT, position, RIGHT
+        elif direction == DOWN:
+            return SERVER_TURN_LEFT, position, RIGHT
+        elif direction == LEFT:
+            return SERVER_TURN_RIGHT, position, UP
+        return SERVER_MOVE, (x+1, y), RIGHT
+
+    elif y > 2:
+        if direction == LEFT:
+            return SERVER_TURN_LEFT, position, DOWN
+        elif direction == RIGHT:
+            return SERVER_TURN_RIGHT, position, DOWN
+        elif direction == UP:
+            return SERVER_TURN_RIGHT, position, RIGHT
+        return SERVER_MOVE, (x, y-1), DOWN
+
+    elif y < 2:
+        if direction == LEFT:
+            return SERVER_TURN_RIGHT, position, UP
+        elif direction == RIGHT:
+            return SERVER_TURN_LEFT, position, UP
+        elif direction == DOWN:
+            return SERVER_TURN_LEFT, position, RIGHT
+        return SERVER_MOVE, (x, y+1), UP
+
+
+def search_box(position, direction, picked):
+    if position == (-2, 2):
+        if direction == UP:
+            return SERVER_TURN_RIGHT, position, RIGHT, False
+        elif direction == RIGHT:
+            return SERVER_TURN_RIGHT, position, DOWN, False
+        elif direction == LEFT:
+            return SERVER_TURN_LEFT, position, DOWN, False
+
+    if not picked:
+        picked = True
+        return SERVER_PICK_UP, position, direction, picked
+
+    elif position == (2, -2):
+        return "Not found!"
+
+    else:
+        x, y = position
+        picked = False
+
+        if x%2 == 0:
+            if y != -2:
+                if direction == RIGHT:
+                    return SERVER_TURN_RIGHT, position, DOWN, picked
+                return SERVER_MOVE, (x, y-1), DOWN, picked
+            if y == -2:
+                if direction == DOWN:
+                    return SERVER_TURN_LEFT, position, RIGHT, True
+                else:
+                    return SERVER_MOVE, (x+1, y), RIGHT, picked
+        else:
+            if y != 2:
+                if direction == RIGHT:
+                    return SERVER_TURN_LEFT, position, UP, picked
+                return SERVER_MOVE, (x, y+1), UP, picked
+            if y == 2:
+                if direction == UP:
+                    return SERVER_TURN_RIGHT, position, RIGHT, True
+                else:
+                    return SERVER_MOVE, (x+1, y), RIGHT, picked
--- 01-tcp-server/server.py
+++ 01-tcp-server/server.py
@@ -15,6 +15,13 @@ class Server:
         print("established connection from {}:{}".format(*address))
         phase = 0
         username_hash = 0
+        first_position = 0, 0
+        second_position = 0, 0
+        actual_position = 0, 0
+        direction = 4
+        picked = False
+        last_action = None
+        recharging = False
 
         try:
             while True:
@@ -26,6 +33,20 @@ class Server:
 
                 print("Received: ", decoded, sep="")
 
+                if decoded == "RECHARGING":
+                    connection.settimeout(TIMEOUT_RECHARGING)
+                    recharging = True
+                    continue
+
+                if decoded == "FULL POWER":
+                    recharging = False
+                    connection.settimeout(TIMEOUT)
+                    continue
+
+                if recharging:
+                    connection.sendall(create_message(SERVER_LOGIC_ERROR))
+                    break
+
                 if phase == 0:
                     username_hash = compute_hash(decoded)
                     print("Sending: ", username_hash, sep="")
@@ -36,10 +57,67 @@ class Server:
                     if (int(decoded) + 65536 - CLIENT_KEY) % 65536 == username_hash:
                         print("Sending: ", SERVER_OK, sep="")
                         connection.sendall(create_message(SERVER_OK))
+
+                        print("Sending: ", SERVER_MOVE, sep="")
+                        connection.sendall(create_message(SERVER_MOVE))
+                        phase = 2
                     else:
                         print("Sending: ", SERVER_LOGIN_FAILED, sep="")
                         connection.sendall(create_message(SERVER_LOGIN_FAILED))
                         break
+
+                elif phase == 2:
+                    if "OK" in decoded:
+                        split = decoded.split(" ")
+                        first_position = int(split[1]), int(split[2])
+                    print("Sending: ", SERVER_MOVE, sep="")
+                    connection.sendall(create_message(SERVER_MOVE))
+                    phase = 3
+
+                elif phase == 3:
+                    if "OK" in decoded:
+                        split = decoded.split(" ")
+                        second_position = int(split[1]), int(split[2])
+                        if second_position != first_position:
+                            actual_position = second_position
+                            direction = get_direction(first_position, second_position)
+                            print("Coordinates = ", actual_position, sep="")
+                            print("Direction = ", direction, sep="")
+                            phase = 4
+                        else:
+                            first_position = second_position
+                            print("Sending: ", SERVER_MOVE, sep="")
+                            connection.sendall(create_message(SERVER_MOVE))
+
+                if phase == 4:
+                    if actual_position == (-2, 2):
+                        phase = 5
+                    else:
+                        split = decoded.split(" ")
+                        actual_position = (int(split[1]), int(split[2]))
+                        move, actual_position, direction = get_next_move(actual_position, direction)
+                        print("Sending: ", move, sep="")
+                        print("Position: ", actual_position, "   Direction: ",  direction, sep="")
+                        last_action = move
+                        connection.sendall(create_message(move))
+
+                if phase == 5:
+                    if picked and decoded != "" and last_action != SERVER_TURN_RIGHT and last_action != SERVER_TURN_LEFT:
+                        phase = 42
+                    else:
+                        # if last_action == SERVER_MOVE and actual_position != (int(split[1]), int(split[2])):
+                        #     actual_position = (int(split[1]), int(split[2]))
+                        #     picked = True
+                        action, actual_position, direction, picked = search_box(actual_position, direction, picked)
+                        print("Sending: ", action, sep="")
+                        print("Position: ", actual_position, "   Direction: ", direction)
+                        last_action = action
+                        connection.sendall(create_message(action))
+
+                if phase == 42:
+                    print("Sending: ", SERVER_LOGOUT, sep="")
+                    connection.sendall(create_message(SERVER_LOGOUT))
+                    break
 
         except timeout:
             print("Timeout!")
@@ -50,6 +128,6 @@ class Server:
     def run(self):
         while True:
             connection, address = self.sock.accept()
-            connection.settimeout(TIMEOUT)
+            connection.settimeout(10000000)
             t = Thread(target=self.handler, args=(connection, address))
             t.start()
